11249ee880edef9cf0d07bdbeaabdc9fabfa0de2
Tooni Mike <osinjesutooni@gmail.com>
2022-11-17 22:06:06 +0100

    Second Commit

--- lib/AllScreens/mainscreen.dart
+++ lib/AllScreens/mainscreen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:rider_appp/AllWidgets/Divider.dart';
+import 'package:geolocator/geolocator.dart';
 
 class MainScreen extends StatefulWidget {
   static const String idScreen = "maniScreen";
@@ -9,12 +14,328 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  Completer<GoogleMapController> _controllerGoogleMap = Completer();
+  late GoogleMapController newGoogleMapController;
+
+  GlobalKey<ScaffoldState> scaffoldkey = new GlobalKey<ScaffoldState>();
+
+// Getting users current position
+
+  late Position currentPosition;
+  var geolocator = Geolocator();
+  double bottomPaddingOfMap = 0;
+
+  Future<Position> locatePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    currentPosition = position;
+
+    LatLng latLatPosition = LatLng(position.latitude, position.longitude);
+
+    CameraPosition cameraPosition =
+        new CameraPosition(target: latLatPosition, zoom: 14);
+    newGoogleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+    return await Geolocator.getCurrentPosition();
+  }
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldkey,
       appBar: AppBar(
-          title: Text("Main Screen"),
-          titleTextStyle: TextStyle(fontFamily: "Brand Bolt")),
+        title: Text("Main Screen"),
+        titleTextStyle: TextStyle(fontFamily: "Brand Bolt"),
+      ),
+      drawer: Container(
+        color: Colors.white,
+        width: 255.0,
+        child: Drawer(
+          child: ListView(
+            children: [
+              //Drawer Header
+              Container(
+                height: 165.0,
+                child: DrawerHeader(
+                  decoration: BoxDecoration(color: Colors.white),
+                  child: Row(children: [
+                    Image.asset(
+                      "images/user_icon.png",
+                      height: 65.0,
+                      width: 65.0,
+                    ),
+                    SizedBox(
+                      width: 16.0,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Profile name",
+                          style: TextStyle(
+                              fontSize: 16.0, fontFamily: "Brand-Bold"),
+                        ),
+                        SizedBox(
+                          height: 6.0,
+                        ),
+                        Text(
+                          "visit profile",
+                          style: TextStyle(fontFamily: "Brand-Bold"),
+                        ),
+                      ],
+                    )
+                  ]),
+                ),
+              ),
+
+              DividerWidget(),
+              SizedBox(
+                height: 12.0,
+              ),
+              //Drawer Body Containers
+              ListTile(
+                leading: Icon(Icons.history),
+                title: Text(
+                  "History",
+                  style: TextStyle(fontSize: 15.0, fontFamily: "Brand-Bold"),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text(
+                  "Visit",
+                  style: TextStyle(fontSize: 15.0, fontFamily: "Brand-Bold"),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.info),
+                title: Text(
+                  "About",
+                  style: TextStyle(fontSize: 15.0, fontFamily: "Brand-Bold"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          GoogleMap(
+            padding: EdgeInsets.only(bottom: bottomPaddingOfMap),
+            mapType: MapType.normal,
+            myLocationButtonEnabled: true,
+            initialCameraPosition: _kGooglePlex,
+            myLocationEnabled: true,
+            zoomGesturesEnabled: true,
+            zoomControlsEnabled: true,
+            onMapCreated: (GoogleMapController controller) {
+              _controllerGoogleMap.complete(controller);
+              newGoogleMapController = controller;
+
+              setState(() {
+                bottomPaddingOfMap = 265.0;
+              });
+
+              locatePosition();
+            },
+          ),
+          //HamburgerButton for Drawer
+          Positioned(
+            top: 45.0,
+            left: 22.0,
+            child: GestureDetector(
+              onTap: () {
+                scaffoldkey.currentState?.openDrawer();
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(22.0),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black,
+                          blurRadius: 6.0,
+                          spreadRadius: 0.5,
+                          offset: Offset(
+                            0.7,
+                            0.7,
+                          ))
+                    ]),
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    Icons.menu,
+                    color: Colors.black,
+                  ),
+                  radius: 20.0,
+                ),
+              ),
+            ),
+          ),
+          //Adding home and work widget
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 0.0,
+            child: Container(
+              height: 300.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(18.0),
+                    topRight: Radius.circular(18.0)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    blurRadius: 16.0,
+                    spreadRadius: 0.5,
+                    offset: Offset(0.7, 0.7),
+                  )
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 24.0, vertical: 18.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 6.0),
+                      Text(
+                        "Hi, there ",
+                        style:
+                            TextStyle(fontSize: 12.0, fontFamily: "Brand-Bold"),
+                      ),
+                      Text(
+                        "Where to? ",
+                        style:
+                            TextStyle(fontSize: 20.0, fontFamily: "Brand-Bold"),
+                      ),
+                      SizedBox(height: 20.0),
+                      Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(5.0),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black54,
+                                blurRadius: 6.0,
+                                spreadRadius: 0.5,
+                                offset: Offset(0.7, 0.7),
+                              )
+                            ],
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Row(
+                              children: [
+                                Icon(
+                                  Icons.search,
+                                  color: Colors.blueAccent,
+                                ),
+                                SizedBox(
+                                  width: 10.0,
+                                ),
+                                Text("Search Drop Off",
+                                    style: TextStyle(
+                                      fontFamily: "Brand-Bold",
+                                    ))
+                              ],
+                            ),
+                          )),
+                      SizedBox(height: 24.0),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.home,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 12.0,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Add Home",
+                                  style: TextStyle(
+                                    fontFamily: "Brand-Bold",
+                                  )),
+                              SizedBox(
+                                height: 4.0,
+                              ),
+                              Text(
+                                "Your living home address",
+                                style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 12.0,
+                                    fontFamily: "Brand-Bold"),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 10.0),
+                      DividerWidget(),
+                      SizedBox(height: 16.0),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.work,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            width: 12.0,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Add work",
+                                  style: TextStyle(
+                                    fontFamily: "Brand-Bold",
+                                  )),
+                              SizedBox(
+                                height: 4.0,
+                              ),
+                              Text(
+                                "Your office address",
+                                style: TextStyle(
+                                    color: Colors.black54,
+                                    fontSize: 12.0,
+                                    fontFamily: "Brand-Bold"),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
